a0ba7d9be72dc9d4a7fd68713da4c1a74eb39e05
Amritpreet Singh <amritpreet98@gmail.com>
2021-08-08 17:10:49 -0500

    style(largecard): width on small screen

--- components/LargeCard/index.tsx
+++ components/LargeCard/index.tsx
@@ -14,7 +14,7 @@ const LargeCard: FC<IProps> = ({
     buttonText,
 }: IProps) => (
     <div className="relative">
-        <div className="relative h-96 min-w-[300px]">
+        <div className="relative h-96 min-w-full">
             <Image
                 className="rounded-2xl"
                 layout="fill"
@@ -23,7 +23,9 @@ const LargeCard: FC<IProps> = ({
             />
         </div>
         <div className="absolute top-32 left-12">
-            <h3 className="text-4xl mb-3 w-64">{title}</h3>
+            <h3 className="text-3xl sm:text-4xl mb-3 w-full sm:w-64">
+                {title}
+            </h3>
             <p>{description}</p>
             <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
                 {buttonText}
