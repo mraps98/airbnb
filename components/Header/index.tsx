b60ebdd0f1ffc94c26c29441a0cdb3c47ce288b0
Amritpreet Singh <amritpreet98@gmail.com>
2021-08-14 13:05:02 -0500

    feat: routing to search page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, FC, useState } from 'react';
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import {
     SearchIcon,
     GlobeAltIcon,
@@ -10,6 +11,7 @@ import {
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker, OnDateRangeChangeProps } from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
 const Header: FC = () => {
     const [searchInput, setSearchInput] = useState('');
@@ -20,6 +22,7 @@ const Header: FC = () => {
         new Date()
     );
     const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const router = useRouter();
 
     const dateRangePickerSelectionRange = {
         startDate: dateRangePickerStartDate,
@@ -49,24 +52,55 @@ const Header: FC = () => {
         setNumberOfGuests(1);
     };
 
+    const handleSearchClick = () => {
+        pushToSearchPage();
+    };
+
+    const handleSearchFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        pushToSearchPage();
+    };
+
+    const pushToSearchPage = () =>
+        !!searchInput &&
+        router.push({
+            pathname: 'search',
+            query: {
+                location: searchInput,
+                startDate: dateRangePickerStartDate.toISOString(),
+                endDate: dateRangePickerEndDate.toISOString(),
+            },
+        });
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
-                <Image
-                    src="https://links.papareact.com/qd3"
-                    layout="fill"
-                    objectFit="contain"
-                    objectPosition="left"
-                />
+                <Link href="/">
+                    <Image
+                        src="https://links.papareact.com/qd3"
+                        layout="fill"
+                        objectFit="contain"
+                        objectPosition="left"
+                    />
+                </Link>
             </div>
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input
-                    className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
-                    placeholder="Start your search"
-                    value={searchInput}
-                    onChange={handleChangeSearchInput}
-                />
-                <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2" />
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm hover:bg-gray-50 transition duration-300">
+                <form
+                    onSubmit={handleSearchFormSubmit}
+                    className="flex items-center flex-grow"
+                >
+                    <input
+                        className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400 transition duration-300"
+                        placeholder="Start your search"
+                        value={searchInput}
+                        required
+                        onChange={handleChangeSearchInput}
+                    />
+                    <SearchIcon
+                        className="h-8 bg-red-400 hover:bg-red-300 transition duration-300 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2"
+                        onClick={handleSearchClick}
+                    />
+                </form>
             </div>
             <div className="flex items-center justify-end space-x-4 text-gray-500">
                 <p className="hidden md:inline cursor-pointer text-right">
@@ -108,7 +142,10 @@ const Header: FC = () => {
                         >
                             Cancel
                         </button>
-                        <button className="text-red-400 flex-grow">
+                        <button
+                            onClick={handleSearchClick}
+                            className="text-red-400 flex-grow"
+                        >
                             Search
                         </button>
                     </div>
