18dadc27da9e6920145bc36a2da2c3242cb2fa23
Amritpreet Singh <amritpreet98@gmail.com>
2021-08-22 12:29:04 -0500

    feat(search): pushing placeholder

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,19 +2,32 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { FC } from 'react';
 import SearchFilter from '../components/SearchFilter';
+import { useRouter } from 'next/dist/client/router';
+import { format } from 'date-fns';
 
 const Search: FC = () => {
+    const router = useRouter();
+    const { location, startDate, endDate, numberOfGuests } = router.query;
+    const formattedStartDate = format(
+        new Date(startDate as string),
+        'dd MMMM yy'
+    );
+    const formattedEndDate = format(new Date(endDate as string), 'dd MMMM yy');
+    const dateRange = `${formattedStartDate} - ${formattedEndDate}`;
     return (
         <>
-            <Header />
+            <Header
+                placeholder={`${location} | ${dateRange} | ${numberOfGuests} guests`}
+            />
 
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
-                    <p className="text-xs">300+ stays for 5 number of guests</p>
+                    <p className="text-xs">
+                        300+ stays - {dateRange} - for {numberOfGuests} guests
+                    </p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">
-                        Stays in New Delhi
+                        Stays in {location}
                     </h1>
-
                     <div className="hidden lg:inline-flex space-x-3 text-gray-800 whitespace-nowrap">
                         <SearchFilter>Cancellation Flexibility</SearchFilter>
                         <SearchFilter>Type of place</SearchFilter>
